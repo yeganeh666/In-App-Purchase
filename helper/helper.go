fd5acc63ba71cdf85fa068f13edd10bca5832cd1
yeganeh nemati <yeganeh.n666@gmail.com>
2020-09-14 17:25:41 +0430

    #3

--- helper/helper.go
+++ helper/helper.go
@@ -1,16 +1,9 @@
 package helper
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 )
 
-func StructToByte(model interface{}) []byte {
-	responseBytes := new(bytes.Buffer)
-	json.NewEncoder(responseBytes).Encode(model)
-	return responseBytes.Bytes()
-}
 func HttpResponse(w http.ResponseWriter, responseStatus int, response []byte) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(responseStatus)
